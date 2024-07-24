255e6cde3614b3a4cb17488c459c9e7fcea79176
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-07-24 17:28:45 +0200

    Add 7 agents

--- scripts/await-ca.sh
+++ scripts/await-ca.sh
@@ -5,5 +5,5 @@ while ! [ -e /ca/var/lib/icinga2/certs/ca.crt ]
 do sleep 1
 done
 
-test -e /var/lib/icinga2/ca || ln -vs {/ca,}/var/lib/icinga2/ca
+test -e /var/lib/icinga2/ca || cp -r {/ca,}/var/lib/icinga2/ca
 exec "$@"
